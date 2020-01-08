084a1ebcbfbfec8f7a863972389488bc811c6ce5
scottfaul <scott.faul@gmail.com>
2020-01-08 14:02:11 +0100

    test out the batch

---
+++ Loop.js
@@ -0,0 +1,12 @@
+let number = 6
+while (number < 10 ) {
+    console.log (number),
+    number = number +1
+}
+
+let letters = ["a", "b", "c", "d", "e"]
+let index = 0
+while (index < letters.length) {
+    console.log(letters[index]),
+    index = index +1
+}
